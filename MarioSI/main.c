73eaccbadf5ed2f7c4d383602b14df269574ccf8
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-19 19:47:52 +0300

    Initial Commit

---
+++ MarioSI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MarioSI
+//
+//  Created by Артем on 6/19/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
